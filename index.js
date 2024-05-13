71dbc5a47e0708ee531e47e0d9df9473693abc78
RohitSathya <156504207+RohitSathya@users.noreply.github.com>
2024-05-13 11:09:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,12 +11,7 @@ app.use(cors())
 app.use('/api/user',UserRoute)
 app.use('/api/admin',AdminRoute)
 app.use('/api/chat',chatRoute)
-io.on("connection",(socket)=>{
-     console.log('conn')
-     socket.on('msges',(data)=>{
-         console.log(data)
-     })
- })
+
 
 mongoose.connect('mongodb+srv://admin:sunsetwest1234RRR@royoapi.3qmdrjq.mongodb.net/Doconnect123?retryWrites').then(()=>{
 
